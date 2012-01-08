9a2a6aa072684028ed4ad3023b71d7271eba29b7
Stone Shi <shi.zaiming@klarna.com>
2012-01-08 20:58:17 +0100

    using rebar

--- src/test/mut.erl
+++ src/test/mut.erl
@@ -8,19 +8,20 @@
         ]).
 
 run() -> 
-    fp("run() ...\n"),
+    fp("#~p: run() ...\n", [?LINE]),
     fun1().
 
 fun1() ->
-    fp("fun1() ...\n"),
+    fp("#~p: fun1() ...\n", [?LINE]),
     fun2().
 
 fun2() ->
-    fp("fun2() ...\n"),
+    fp("#~p: fun2() ...\n", [?LINE]),
     mymod:myfun(whatever).
 
 fp(Str) ->
-    io:format(Str).
+    io:put_chars(Str).
+
 fp(Str, Fmt) ->
-    io:format(Str, Fmt).
+    fp(io_lib:format(Str, Fmt)).
 
