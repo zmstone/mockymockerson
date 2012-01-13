89688423656f83f90871d7845822f3b3bcf55ab7
Stone Shi <shi.zaiming@klarna.com>
2012-01-13 22:54:25 +0100

    add a test suite for parse, ready to refactory

---
+++ em/test/mockymockerson_parse_SUITE.erl
@@ -0,0 +1,17 @@
+
+-module(mockymockerson_parse_SUITE).
+
+-include("mockymockerson.hrl").
+-include("../mockymockerson_em_private.hrl").
+
+-record(a, {b, c, d, e, f}).
+-record(b, {c, d, e, f}).
+-record(x, {y :: integer(),
+            z :: string()}).
+
+all_test() ->
+    ?assertEqual(record_info(fields, a), '$MOCKYMOCKERSON_RECORD_FIELDS'(a)),
+    ?assertEqual(record_info(fields, b), '$MOCKYMOCKERSON_RECORD_FIELDS'(b)),
+    ?assertEqual(record_info(fields, x), '$MOCKYMOCKERSON_RECORD_FIELDS'(x)),
+    ok.
+
